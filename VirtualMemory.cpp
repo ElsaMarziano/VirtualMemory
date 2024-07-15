e3bc91326cff6ac3a0d7930253fef94fc77f691d
e342791191 <elsa.sebagh@mail.huji.ac.il>
2024-07-15 14:25:50 +0300

    ??

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -1,89 +1,110 @@
-#include VirtualMemory.h
-#include MemoryConstants.h
-#include PhysicalMemory.h
+#include "VirtualMemory.h"
+#include "MemoryConstants.h"
+#include "PhysicalMemory.h"
+#include <cstdint>
 
-void VMinitialize ()
-{
-  PMwrite (0, 0);
-//  Put root at 0
-//  for (uint64_t i = 1; i < NUM_FRAMES; i++) {
-//    PMwrite(i, 0);
-//  }
-}
-
-int VMread (uint64_t virtualAddress, word_t *value)
-{
-  uint64_t physicalAddress = mapAddress (virtualAddress);
-  PMread (physicalAddress, value);
-  return SUCCESS;
-}
-
-int VMwrite (uint64_t virtualAddress, word_t value)
-{
-  uint64_t physicalAddress = mapAddress (virtualAddress);
-  PMwrite (physicalAddress, value);
-  return SUCCESS;
-}
 
 uint64_t mapAddress (uint64_t virtualAddress)
 {
-  const offset_mask = (1 << OFFSET_WIDTH) - 1;
+  auto offset_mask = (1 << OFFSET_WIDTH) - 1;
   word_t address = 0;
 
   for (int i = TABLE_DEPTH; i >= 0; i--)
   {
-    uint64_t pageIndex = (virtualAddress >> (OFFSET_WIDTH * i)) & offset_mask;
-    if (i == 0) return address * PAGE_SIZE + pageIndex;
-    PMread (address * PAGE_SIZE + pageIndex, address);
+    uint64_t current_offset = (virtualAddress >> (OFFSET_WIDTH * i)) &
+                              offset_mask;
+    if (i == 0) return address * PAGE_SIZE + current_offset;
+    PMread (address * PAGE_SIZE + current_offset, address);
     if (address == 0) // Page fault
     {
-      const f1 = findFrame (address * PAGE_SIZE + pageIndex);
-      if (i > 1) PMWrite (f1, 0); // ???
-      PMwrite (address * PAGE_SIZE + pageIndex, f1);
+      auto f1 = findFrame (address * PAGE_SIZE + current_offset, address);
+      if (i > 1)
+      {
+        for (int j = 0; j < NUM_PAGES; j++)
+        {
+          PMWrite (f1 + j, 0);
+        }
+      }
+      PMwrite (address * PAGE_SIZE + current_offset, f1);
       address = f1;
     }
   }
-  return address;
 }
 
-uint64_t findFrame (uint64_t pageIndex)
+uint64_t findFrame (uint64_t pageIndex, uint64_t parentFrameIndex
 {
   uint64_t maxFrameIndex = 0;
-  const frameIndex = dfs (0, 0, &maxFrameIndex);
+//  TODO Link frameIndex to parent
+  auto frameIndex = dfs (0, 0, &maxFrameIndex, parentFrameIndex);
   if (frameIndex == -1)
   {
     if (maxFrameIndex + 1 < NUM_PAGES)
     {
-      frameIndex = maxFrameIndex
-    } else {
-        for(uint64_t i = 0; i < NUM_FRAMES; i++)
-        {
+      frameIndex = maxFrameIndex;
+      for (uint64_t i = 0; i < NUM_PAGES; i++)
+      {
+        PMwrite (frameIndex + i, 0);
+      }
+    }
+    else
+    {
+      for (uint64_t i = 0; i < NUM_FRAMES; i++)
+      {
 
 //          PMevict (i, i);
-        }
+      }
     }
   }
   PMrestore (frameIndex, pageIndex)
   return frameIndex;
 }
 
-uint64_t dfs (uint64_t i, int depth, uint64_t *maxFrameIndex)
+uint64_t dfs (uint64_t i, int depth, uint64_t *maxFrameIndex, uint64_t
+previousFrame, uint64_t fatherFrameIndex)
 {
+  word_t currentFrame = 0;
   if (depth >= TABLES_DEPTH) return -1;
   for (uint64_t j = 0; j < PAGE_SIZE; j++)
   {
-    if (i * PAGE_SIZE + j > &maxFrameIndex) *maxFrameIndex = i * PAGE_SIZE + j;
-    word_t value;
-    PMread (i * PAGE_SIZE + j, value);
-    if (value != 0)
+    PMread (i * PAGE_SIZE + j, &currentFrame);
+    if (currentFrame > *maxFrameIndex)
     {
-      const nextAddress = dfs (value, depth + 1);
+      *maxFrameIndex = currentFrame;
+    }
+    if (currentFrame != 0 || i * PAGE_SIZE == previousFrame)
+    {
+      auto nextAddress = dfs (currentFrame, depth + 1, maxFrameIndex,
+                               previousFrame, i * PAGE_SIZE + j);
       if (nextAddress != -1)
       {
-        PMwrite (i * PAGE_SIZE + j, 0);
         return nextAddress;
       }
+      else return -1;
     }
   }
-  return i * PAGE_SIZE;;
+  PMwrite (fatherFrameIndex, 0);
+  return i * PAGE_SIZE;
+}
+
+void VMinitialize ()
+{
+  for (int i = 0; i < NUM_PAGES; i++)
+  {
+    PMwrite (i, 0);
+
+  }
+}
+
+int VMread (uint64_t virtualAddress, word_t *value)
+{
+  uint64_t physicalAddress = mapAddress (virtualAddress);
+  PMread (physicalAddress, value);
+  return 1;
+}
+
+int VMwrite (uint64_t virtualAddress, word_t value)
+{
+  uint64_t wordIndex = mapAddress (virtualAddress);
+  PMwrite (wordIndex, value);
+  return 1;
 }
